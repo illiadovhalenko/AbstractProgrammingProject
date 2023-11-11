3ccaf7fb6134e6e2eacdcd74ca34203e103abf89
mac <illiadovgalenko@gmail.com>
2023-11-11 16:10:12 +0100

    environment setup

---
+++ build/CMakeFiles/zad_domowe_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad_domowe_2.dir/main.cpp.o"
+  "CMakeFiles/zad_domowe_2.dir/main.cpp.o.d"
+  "zad_domowe_2"
+  "zad_domowe_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zad_domowe_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <boost/filesystem/operations.hpp>
+int main(int argc, char *argv[]) {
+    std::cout << "The size of " << boost::filesystem::absolute(argv[0])
+              << " is " << boost::filesystem::file_size(argv[0]) << '\n';
+    return 0;
+}
